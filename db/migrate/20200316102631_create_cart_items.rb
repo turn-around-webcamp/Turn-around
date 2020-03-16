6791ad0edcd1b92080080ac06319c1f0706d7812
shingo tanaka <stenstenktensten@gmail.com>
2020-03-16 10:45:08 +0000

    カート内商品と商品のモデル、コントローラを追加しました

---
+++ db/migrate/20200316102631_create_cart_items.rb
@@ -0,0 +1,11 @@
+class CreateCartItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cart_items do |t|
+      t.integer :total_volume
+      t.integer :member_id
+      t.integer :item_id
+
+      t.timestamps
+    end
+  end
+end
