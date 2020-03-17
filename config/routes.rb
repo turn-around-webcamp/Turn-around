5012c4f97d35f4d3924dbc66219ce96384127881
Tsutomu19 <mdhd.to@gmail.com>
2020-03-17 07:05:35 +0000

    登録情報編集画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
    root 'homes#top'
-   resources :users, only:[:show]
+   resources :users, only:[:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
