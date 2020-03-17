8b777580d70e32ad9dab9cd71382088899823990
Tsutomu19 <mdhd.to@gmail.com>
2020-03-17 08:34:00 +0000

    email,link_to修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_03_17_062816) do
-
+ActiveRecord::Schema.define(version: 2020_03_17_080940) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "total_volume"
