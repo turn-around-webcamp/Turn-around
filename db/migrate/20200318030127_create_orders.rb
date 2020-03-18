4fd23ebff6f360eefc7f69cd6dfe813297cdc419
shingo tanaka <stenstenktensten@gmail.com>
2020-03-18 03:47:47 +0000

    注文モデルを追加しました

---
+++ db/migrate/20200318030127_create_orders.rb
@@ -0,0 +1,16 @@
+class CreateOrders < ActiveRecord::Migration[5.2]
+  def change
+    create_table :orders do |t|
+      t.integer :payment_method,default: 0, null: false
+      t.integer :postage, null: false
+      t.integer :charge, null: false
+      t.integer :status, null: false
+      t.string :neme_address, null: false
+      t.string :address, null: false
+      t.string :postal_code, null: false
+      t.integer :user_id, null: false
+
+      t.timestamps
+    end
+  end
+end
