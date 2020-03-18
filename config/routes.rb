232549ffaf752634908f79b829abbc8a54ddc04d
shingo tanaka <stenstenktensten@gmail.com>
2020-03-18 06:17:24 +0000

    確認用push

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :items,only: [:show,:index]
   resources :cart_items,only: [:index,:create,:update,:destroy]
   delete 'cart_items' => 'cart_items#destroy_all'
+  resources :orders, only: [:show,:index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
