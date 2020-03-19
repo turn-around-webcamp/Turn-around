5d283d76873496291ad8e6857c089491ee549c2f
takedatsutomu <mdhd.to@gmail.com>
2020-03-19 14:40:07 +0900

    [Add]配送先更新画面、更新

--- app/controllers/delivery_addresses_controller.rb
+++ app/controllers/delivery_addresses_controller.rb
@@ -8,19 +8,34 @@ class DeliveryAddressesController < ApplicationController
         # ※注意※Delivery_address.allと書かない。RoadErrorになる。
     end
 
+    def show
+
+    end
+
     def create
         @deliver = DeliveryAddress.new(delivery_address_params)
         # ストロングパラメータも同時に定義してあげる事。NameErrorになる。
         @deliver.user_id = current_user.id
         @deliver.save
         redirect_to delivery_addresses_path
+        # リダイレクト先未決定
     end
     def edit
         @deliver = DeliveryAddress.find(params[:id])
     end
     def update
+        @deliver = DeliveryAddress.find(params[:id])
+        # 下記if文を書かないとRoutingErrorが起き、更新できない。
+        if @deliver.update(delivery_address_params)
+        redirect_to delivery_addresses_path
+        else
+            render "edit"
+        end
     end
     def destroy
+        delivery = DeliveryAddress.find(params[:id])
+        delivery.destroy
+        redirect_to delivery_addresses_path
     end
 
     private
