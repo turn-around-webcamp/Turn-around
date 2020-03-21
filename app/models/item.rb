5b365e0a377836c72c11cc9bc26c55c9ebb17a4d
shingo tanaka <stenstenktensten@gmail.com>
2020-03-21 08:28:15 +0000

    カート機能画像以外表示完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,9 @@ class Item < ApplicationRecord
   has_many :order_items
   belongs_to :category
   mount_uploader :image, ImagesUploader
+  attachment :image, destroy: false
+
+  def post_tax_price#税込価格を定義している
+    non_tax_price*1.1
+  end
 end
