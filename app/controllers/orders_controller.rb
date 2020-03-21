61e3c7a67e145c779e7bf71220af8ac695a8b4c7
takumi-shibata <241448ta@gmail.com>
2020-03-21 05:21:26 +0000

    確認画面作成途中

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   def new
   	 @order = Order.new
+     @user = current_user
   end
   
   def index
@@ -11,15 +12,20 @@ class OrdersController < ApplicationController
     #@order = Order.find(params[:id])
   end
   def confirm
-  	 @order = Order.new
+  	 @order = Order.new(order_params)
   end
 
   def create
-	  @order = Order.new
-	  redirect_to action: :finish
+	   @order = Order.new(order_params)
+     @order.save
+	   redirect_to action: :finish
   end
 
   def finish
   end
-  
+
+  private
+  def order_params
+     params.require(:order).permit(:postage, :charge, :payment_method, :neme_address, :address, :postal_code)
+  end
 end
