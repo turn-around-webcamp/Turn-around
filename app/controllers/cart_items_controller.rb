93c0306158277f1b1c71e07d891fc41ab2152e89
shingo tanaka <stenstenktensten@gmail.com>
2020-03-22 11:08:34 +0000

    商品詳細画面作成完了

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -2,21 +2,26 @@ class CartItemsController < ApplicationController
   def index
     @cart_items = current_user.cart_items.all
   end
+
   def create
-    @cart_item = current_user.cart_items.new(cart_item_params)
-    @cart_item.save
-    redirect_to new_order_path(@cart_item.id)
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+   if @cart_item.save
+    redirect_to cart_items_path
+   end
   end
+
   def update
     @cart_item = current_user.cart_items.find(params[:id])
     @cart_item.update(cart_item_params)
     redirect_to cart_items_path
   end
+
   def destroy
     @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
     redirect_to cart_items_path
   end
+
   def destroy_all
     @cart_items = current_user.cart_items.all
     @cart_items.destroy_all
