e47f2e368c15edf15c9a254a5930148777dfd762
shingo tanaka <stenstenktensten@gmail.com>
2020-03-23 02:04:47 +0000

    カート削除機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ get 'users/:id/leave' => 'users#leave'
   resources :users, only:[:show,:edit,:update,:destroy]
   resources :items,only: [:show,:index]
   resources :cart_items,only: [:index,:create,:update,:destroy]
-  delete 'cart_items' => 'cart_items#destroy_all'
+  delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
   resources :orders, only: [:show,:index, :new, :create] do
   	   collection do
   	      post 'confirm'
