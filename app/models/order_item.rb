0819448b2743484b427b20770957ad16d2049f90
takedatsutomu <mdhd.to@gmail.com>
2020-03-27 16:47:26 +0900

    [AddAdmin]ステータスの再編集

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,6 +1,6 @@
 class OrderItem < ApplicationRecord
     belongs_to :item
     belongs_to :order
-    enum status:{着手不可: 0, 制作待ち: 1, 制作中: 2, 制作完了: 3}
+    enum status:{着手不可: 0, 制作待ち: 1, "制作中": 2, "制作完了": 3}
 # ここだけに数字。データベースの中身は数字で保存
 end
